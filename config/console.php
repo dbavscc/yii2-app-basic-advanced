76fc13409d2eb6f89783b32dde22d6de360b09fc
DBA <dab.vscc@yandex.ru>
2015-06-28 21:40:19 +0000

    big changes

---
+++ config/console.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'components' => [
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => [],
+];

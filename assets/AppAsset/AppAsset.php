3fc4509de7a5f45f3d3d710da7304021f78c24ba
dab <dab.vscc@yandex.ru>
2016-01-18 14:43:15 +0300

    move assets to assets folder

--- assets/AppAsset/AppAsset.php
+++ assets/AppAsset/AppAsset.php
@@ -5,7 +5,7 @@
  * @license http://www.yiiframework.com/license/
  */
 
-namespace app\assets;
+namespace app\assets\AppAsset;
 
 use yii\web\AssetBundle;
 
@@ -15,8 +15,7 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = "@app/assets/AppAsset/src";
     public $css = [
         'css/site.css',
     ];

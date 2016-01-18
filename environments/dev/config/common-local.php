06c0aa4aaaafb026381da368bbe45089a466f522
dab <dab.vscc@yandex.ru>
2016-01-18 14:49:10 +0300

    move mail templates folder to views/mailer

--- environments/dev/config/common-local.php
+++ environments/dev/config/common-local.php
@@ -10,7 +10,7 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            //'viewPath' => '@app/mail',
+            'viewPath' => '@app/views/mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
